7d1b9aa6c7efbf23d3f9b99d8b4fb0381769c72e
Jeff Enninful <jeffrey.enninful@agilysys.com>
2016-11-21 10:44:32 -0800

    change app name

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,16 +1,19 @@
 export class MainController {
-  constructor ($timeout, webDevTec, toastr) {
+  constructor ($timeout, $log, webDevTec, toastr) {
     'ngInject';
 
     this.awesomeThings = [];
     this.classAnimation = '';
     this.creationDate = 1479701674992;
     this.toastr = toastr;
+    this.$log = $log;
 
     this.activate($timeout, webDevTec);
   }
 
   activate($timeout, webDevTec) {
+    this.$log.debug('angular ', angular);
+
     this.getWebDevTec(webDevTec);
     $timeout(() => {
       this.classAnimation = 'rubberBand';
